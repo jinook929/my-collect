f6748f6a69a5e022ab002a7e8ecf2ac763cc9a10
jinook929 <jinook929@gmail.com>
2021-01-13 19:04:38 -0500

    commit tree 865c1701beb4288b8ea129ec7d0feb55484fe9fb

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,20 @@
+def my_collect(arr)
+    if block_given?
+        i = 0
+        new_arr = []
+        while i < arr.length
+            new_arr.push(yield(arr[i]))
+            i += 1
+        end
+        new_arr
+    end
+end
 
+# array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
 
+# p my_collect(array) do |name|
+#   name.split(" ").first
+# end 
+
+# p my_collect(array) {|name| name}
+# p my_collect(array) {|name| "Hey, #{name.split(" ").first}!" }
